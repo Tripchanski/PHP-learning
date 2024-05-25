80f5af359dbd4b0c39c7120c7aad1ccd5827f84b
Tripchanski <arturtripking01@gmail.com>
2024-05-25 14:02:27 +0300

    database and a little more

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
@@ -42,4 +44,18 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    protected function name(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => Str::upper($value)
+        );
+    }
+
+    protected function password(): Attribute
+    {
+        return Attribute::make(
+            set: fn ($value) => bcrypt($value)
+        );
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +17,81 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+
+    // $users = DB::table('users')->get();
+    // $user = DB::table('users')->where('id', 2)->get();
+
+
+
+    // $users = DB::select('select * from Users');
+    // $user = DB::select('select * from Users where id=1');
+    // $user_by_email = DB::select('select * from Users where email=?', ['beb@gmail.com']);
+    // $user = User::where('id', 2)->get();
+    $user = User::find(7);
+    $users = User::all();
+
+
+
+    // create user
+    // $create_user = DB::insert('insert into Users (name, email, password) values (?, ?, ?)', [
+    //     'Artur',
+    //     'beb@gmail.com',
+    //     'password',
+    // ]);
+    // $create_user = DB::table('users')->insert([
+    //     'name' => 'Ar',
+    //     'email' => 'beb@gmail.com',
+    //     'password' => '12345678',
+    // ]);
+
+    // $create_user = User::create([
+    // 'name' => 'Ar',
+    // 'email' => 'abcdefg@tripchanski.com',
+    // 'password' => '12345678',
+    // ]);
+
+
+
+    // update user
+    // $update_user = DB::update("update Users set email=? where id=?", [
+    //     'abc@tripchanski.com',
+    //     1,
+    // ]);
+    // $update_user = DB::table('users')->where('id', 4)->update([
+    //     'email' => 'abc@tripchanski.com',
+    // ]);
+    // $update_user = User::find(5);
+    // $update_user->update([
+    //     'email' => 'bebeb@tripchanski.com',
+    // ]);
+
+
+
+    // delete user
+    // $delete_user = DB::delete("delete from Users where id=?", [
+    // 1,
+    // ]);
+    // $delete_user = DB::table('users')->where('id', 4)->delete();
+    // $delete_user = User::find(2);
+    // $delete_user->delete();
+
+
+    // dd($users);
+    // dd($user);
+
+
+    dd($users);
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
